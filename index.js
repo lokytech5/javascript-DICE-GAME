f92177df8deeea66a04d090dad432a12d790b8c6
lokytech <lokytech5@gmail.com>
2023-02-28 21:04:07 +0100

    initial commit

---
+++ index.js
@@ -0,0 +1,24 @@
+// First images
+let randomNumber1 = Math.floor(Math.random() * 6) + 1;
+let randomDiceImage = "dice" + randomNumber1 + ".png";
+let randomImageSource = "images/" + randomDiceImage;
+let image1 = document.querySelectorAll("img")[0];
+image1.setAttribute("src", randomImageSource);
+
+//second images
+
+let randomNumber2 = Math.floor(Math.random() * 6) + 1;
+let randomDiceImage2 = "dice" + randomNumber2 + ".png";
+let randomImageSource2 = "images/" + randomDiceImage2;
+
+let image2 = document.querySelectorAll("img")[1];
+
+image2.setAttribute("src", randomImageSource2);
+
+if (randomNumber1 === randomNumber2) {
+    document.querySelector("h1").innerHTML = "Drawn!";
+} else if (randomNumber1 > randomNumber2) {
+    document.querySelector("h1").innerHTML = "Player1 wins!"; 
+  } else if (randomNumber1 < randomNumber2) {
+    document.querySelector("h1").innerHTML = "player2 wins!";
+  }
